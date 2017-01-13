0e2dd7bd7b33330daa0282132015fcfe4f032b33
Daniel Boakye <boatengfrankenstein@gmail.com>
2017-01-13 12:19:23 -0500

    modified manifest

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,8 +1,3 @@
 //= link_tree ../images
 //= link_directory ../javascripts .js
 //= link_directory ../stylesheets .css
-# SASS
-*= require devise_bootstrap_views
-
-# LESS
-*= require devise_bootstrap_views_less
